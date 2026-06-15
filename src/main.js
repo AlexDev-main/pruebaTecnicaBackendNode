75cbe8efc41b6e2aa511d4d4c52519fccfb73a08
carlos123asd <carlos-medin@hotmail.com>
2026-06-15 23:25:45 +0200

    ADD: definir el inicio del servidor y eventos para poder cerrar recursos y conexion - main solo para el comportamiento y estado del servidor

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import app from "./interfaces/http/app.js";
+import { env } from "./infrastructure/config/env.js";
+import { connectMongo, disconnectMongo } from "./infrastructure/database/mongo/mongoConnection.js";
+
+const startServer = async () => {
+  try {
+    await connectMongo();
+
+    const server = app.listen(env.port, () => {
+      console.log(`Server running on port ${env.port}`);
+    });
+
+    const shutdown = async () => {
+      console.log("Shutting down application...");
+
+      server.close(async () => {
+        await disconnectMongo();
+
+        process.exit(0);
+      });
+    };
+
+    process.on("SIGINT", shutdown); //Captura Ctrl+C
+    process.on("SIGTERM", shutdown); //Captura cuando detenemos contenedor de Docker
+
+  } catch (error) {
+    console.error("Application startup failed:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
