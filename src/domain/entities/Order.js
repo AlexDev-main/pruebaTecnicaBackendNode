36cfe65df843e9f388221efb559e5956cad41191
carlos123asd <carlos-medin@hotmail.com>
2026-06-16 00:53:23 +0200

    UPDATE: añadir una nueva entidad OrderItem que representa informacion individual y de inventario de un producto en una orden, naming

--- src/domain/entities/Order.js
+++ src/domain/entities/Order.js
@@ -1,15 +1,15 @@
 export class Order {
   constructor({
     id,
-    user_id,
-    order_date,
-    total_amount,
+    userId,
+    orderDate,
+    totalAmount,
     products,
   }) {
     this.id = id;
-    this.user_id = user_id;
-    this.order_date = order_date;
-    this.total_amount = total_amount;
-    this.products = products;
+    this.userId = userId;
+    this.orderDate = orderDate;
+    this.totalAmount = totalAmount;
+    this.products = products ?? [];
   }
 }
---
+++ src/domain/entities/OrderItem.js
@@ -0,0 +1,11 @@
+export class Item {
+    constructor({
+    productId,
+    quantity,
+    unitPrice,
+  }) {
+    this.productId = productId;
+    this.quantity = quantity;
+    this.unitPrice = unitPrice;
+  }
+}
